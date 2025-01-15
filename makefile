a517bf179f0570f4769099aaa06495d566cae15b
Naomi Steinmetz <nsteinmetz50@stuy.edu>
2025-01-15 17:03:42 -0500

    debugging persistent

--- makefile
+++ makefile
@@ -1,18 +1,18 @@
 all: client server
-client: basic_client.o persistant_server.o
-	gcc -o client basic_client.o persistant_server.o
+client: basic_client.o pipe_networking.o
+	gcc -o client basic_client.o pipe_networking.o
 
-server: basic_server.o persistant_server.o
-	gcc -o server basic_server.o persistant_server.o
+server: persistant_server.o pipe_networking.o
+	gcc -o server persistant_server.o pipe_networking.o
 
 basic_client.o: basic_client.c pipe_networking.h
 	gcc -c basic_client.c
 
-basic_server.o: basic_server.c pipe_networking.h
-	gcc -c basic_server.c
-
 persistant_server.o: persistant_server.c pipe_networking.h
 	gcc -c persistant_server.c
+
+pipe_networking.o: pipe_networking.c pipe_networking.h
+	gcc -c pipe_networking.c
 
 clean:
 	rm *.o
--- persistant_server.c
+++ persistant_server.c
@@ -17,196 +17,22 @@ static void sighandler(int signo) {
   if ( signo == SIGINT ){
     unlink(WKP);
   }
-}
-
-int err(){
-  printf("errno %d\n",errno);
-  printf("%s\n",strerror(errno));
-  exit(1);
-}
-
-int randomNum(){
-  int x;
-  int bytes;
-  int r_file = open("/dev/random", O_RDONLY, 0);
-  if (r_file == -1)err();
-  bytes = read(r_file, &x, 4);
-  if (bytes == -1){
-      err();
+  if (signo == SIGPIPE){
+    int x = 0;
+    x++;
   }
-  return x;
-}
-
-int randomHundred(){
-  int x;
-  int bytes;
-  int r_file = open("/dev/random", O_RDONLY, 0);
-  if (r_file == -1)err();
-  bytes = read(r_file, &x, 4);
-  if (bytes == -1){
-      err();
-  }
-  x = abs(x) % 101;
-  return x;
-}
-
-//UPSTREAM = to the server / from the client
-//DOWNSTREAM = to the client / from the server
-/*=========================
-  server_setup
-
-  creates the WKP and opens it, waiting for a  connection.
-  removes the WKP once a connection has been made
-
-  returns the file descriptor for the upstream pipe.
-  =========================*/
-int server_setup() {
-  int from_client = 0;
-  char line[256];
-  int piper;
-  unlink(WKP);
-  piper = mkfifo(WKP, 0644); //make pipe
-  if (piper == -1){
-    err();
-  }
-  int fdw;
-  fdw = open(WKP, O_RDONLY); //open WKP
-  if (fdw < 0) {
-    perror("not able to read wkp ");
-    err();
-  }
-  from_client = fdw;
-  remove(WKP); //remove
-  return from_client;
-}
-
-/*=========================
-  server_handshake
-  args: int * to_client
-
-  Performs the server side pipe 3 way handshake.
-  Sets *to_client to the file descriptor to the downstream pipe (Client's private pipe).
-
-  returns the file descriptor for the upstream pipe (see server setup).
-  =========================*/
-int server_handshake(int *to_client) {
-    int from_client = 0;
-    while (1){
-        signal(SIGINT, sighandler);
-        char priv[50];
-        int fdw = server_setup();
-        /*int fdw = open(from_client, O_WRONLY);
-        if (fdw == -1){
-            err();
-        }*/
-        from_client = fdw;
-        int r = read(fdw, priv, sizeof(priv)); //read syn
-        if (r == -1){
-            err();
-        }
-        printf("Client PID, received by server: %s\n", priv);
-        int down;
-        down = open(priv, O_WRONLY); //open pp
-        if (down == -1){
-            err();
-        }
-        *to_client = down;
-        int num = randomNum();
-        printf("Intended SYN: %d\n", num);
-        int w = write(down, &num, sizeof(num)); //send syn ack
-        if (w == -1){
-            err();
-        }
-        r = read(fdw, &num, sizeof(num));
-        if (r == -1){
-            err();
-        }
-        printf("Received ack: %d\n", num);
-
-        while (1){
-            num = randomHundred();
-            w = write(down, &num, sizeof(num)); //send syn ack
-            if (w == -1){
-            err();
-            }
-            sleep(1);
-        }
-        close(from_client);
-        close(*to_client);
-    }
-  return from_client;
 }
 
 
-/*=========================
-  client_handshake
-  args: int * to_server
+int main() {
+  int to_client;
+  int from_client;
+  signal(SIGINT, sighandler);
+  signal(SIGPIPE, sighandler);
 
-  Performs the client side pipe 3 way handshake.
-  Sets *to_server to the file descriptor for the upstream pipe.
-
-  returns the file descriptor for the downstream pipe.
-  =========================*/
-int client_handshake(int *to_server) {
-  char priv[50]; 
-  int num = getpid(); 
-  sprintf(priv, "%d", num); 
-  int privat = mkfifo(priv, 0644); //make pp
-  if (privat == -1){
-    err();
-  }
-
-  int fdw;
-  fdw = open(WKP, O_WRONLY); //open WKP
-  if (fdw == -1){
-    perror("couldn't write to wkp");
-    err();
-  }
-  *to_server = fdw;
-  printf("Intended PID: %s\n", priv);
-  int w = write(fdw, priv, sizeof(priv)); //write pp to wkp
-  if (w == -1){
-    err();
-  }
-  int rr;
-  rr = open(priv, O_RDONLY); //open pp
-  if (rr == -1){
-    err();
-  }
-  remove(priv); //delete pp
-  int signal;
-  int reading = read(rr, &signal, sizeof(signal));
-  if (reading == -1){
-    err();
-  }
-  printf("Received syn ack: %d\n", signal);
-  signal++;
-  w = write(fdw, &signal, sizeof(signal)); //ack
-  if (w == -1){
-    err();
-  }
-  
   while (1){
-    reading = read(rr, &signal, sizeof(signal));
-    if (reading == -1){
-      err();
-    }
-    printf("New Int: %d\n", signal);
+    from_client = server_handshake( &to_client );
+    close(to_client);
+    close(from_client);
   }
-  int from_server = rr;
-  return from_server;
-}
-
-
-/*=========================
-  server_connect
-  args: int from_client
-
-  handles the subserver portion of the 3 way handshake
-
-  returns the file descriptor for the downstream pipe.
-  =========================*/
-int server_connect(int from_client) {
-  int to_client  = 0;
-  return to_client;
 }
